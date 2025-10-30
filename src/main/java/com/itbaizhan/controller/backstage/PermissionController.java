0c879674daddf0bad18f430c2c36e2a55ff27a81
hello <3113942885@qq.com>
2025-10-31 06:53:32 +0800

    自己写的权限管理

---
+++ src/main/java/com/itbaizhan/controller/backstage/PermissionController.java
@@ -0,0 +1,55 @@
+package com.itbaizhan.controller.backstage;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itbaizhan.pojo.Permission;
+import com.itbaizhan.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/backstage/permission")
+public class PermissionController {
+    @Autowired
+    private PermissionService permissionService;
+
+    @RequestMapping("/all")
+    public ModelAndView all(@RequestParam(defaultValue = "1")int page,
+                            @RequestParam(defaultValue = "10") int size){
+        Page<Permission> permissionPage = permissionService.findPage(page, size);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("permissionPage",permissionPage);
+        modelAndView.setViewName("backstage/permission_all");
+        return modelAndView;
+    }
+
+    @RequestMapping("/add")
+    public String add(Permission permission){
+        permissionService.add(permission);
+        return "redirect:/backstage/permission/all";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(Integer pid){
+        permissionService.delete(pid);
+        return  "redirect:/backstage/permission/all";
+    }
+
+    @RequestMapping("/edit")
+    public ModelAndView edit(Integer pid){
+        Permission permission = permissionService.selectById(pid);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("permission",permission);
+        modelAndView.setViewName("backstage/permission_edit");
+        return modelAndView;
+    }
+
+    @RequestMapping("/update")
+    public String update(Permission permission){
+        permissionService.update(permission);
+        return "redirect:/backstage/permission/all";
+    }
+
+}
--- src/main/java/com/itbaizhan/service/PermissionService.java
+++ src/main/java/com/itbaizhan/service/PermissionService.java
@@ -1,11 +1,13 @@
 package com.itbaizhan.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itbaizhan.bean.PermissionWithStatus;
 import com.itbaizhan.mapper.PermissionMapper;
 import com.itbaizhan.pojo.Permission;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +31,29 @@ public class PermissionService {
             permissionWithStatusList.add(permissionWithStatus);
         }
         return permissionWithStatusList;
+    }
+
+    // 分页查询权限
+    public Page<Permission> findPage(int page, int size) {
+        return permissionMapper.selectPage(new Page<>(page,size),null);
+    }
+
+    //新增权限
+    public void add(Permission permission) {
+        permissionMapper.insert(permission);
+    }
+
+    public void delete(Integer pid) {
+        permissionMapper.deleteById(pid);
+    }
+
+    // 查询权限
+    public Permission selectById(Integer pid) {
+        return permissionMapper.selectById(pid);
+    }
+
+    // 修改权限
+    public void update(Permission permission) {
+        permissionMapper.updateById(permission);
     }
 }
